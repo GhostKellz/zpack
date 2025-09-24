83a690f28e9900cff6bb58df9b0cd3761e016a28
GhostKellz <ckelley@ghostkellz.sh>
2025-09-24 19:25:22 -0400

    Added Disclaimer

--- .zig-cache/c/9110444cdb03392f35e1273cad2d360d/options.zig
+++
@@ -1,6 +0,0 @@
-pub const enable_lz77: bool = true;
-pub const enable_rle: bool = false;
-pub const enable_streaming: bool = true;
-pub const enable_simd: bool = true;
-pub const enable_threading: bool = true;
-pub const enable_validation: bool = true;
